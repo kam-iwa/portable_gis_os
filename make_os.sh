642c0615c30e44190a3ef1ac73b8f08747847874
Kamil Iwaniuk <kamiliwaniukkamil@gmail.com>
2021-11-17 22:02:47 +0100

    POPRAWKI CD

--- make_os.sh
+++ make_os.sh
@@ -12,17 +12,14 @@ chmod 700 ./auto/config
 # ZMIANY W MOTYWIE GRAFICZNYM SYSTEMU
 cp -ar ../files/includes.chroot/ config/
 cp -ar ../files/includes.binary/ config/
-#cp -ar ../files/hooks/ config/
+cp -ar ../files/hooks/2137-depedencies.chroot config/hooks/live
 
 # KOLEJNE KROKI TWORZENIA SYSTEMU OPERACYJNEGO
-# 1. DODANIE INSTALATORA
-# echo debian-installer-launcher >> config/package-lists/installer.list.chroot
-
-# 2. DODANIE INTERFEJSU GRAFICZNEGO
+# 1. DODANIE INTERFEJSU GRAFICZNEGO
 echo lxde-core >> config/package-lists/gui.list.chroot
 
-# 3. DODANIE POZOSTALYCH PAKIETOW
-echo "zip unzip" >> config/package-lists/software.list.chroot
+# 2. DODANIE POZOSTALYCH PAKIETOW
+echo "zip unzip firefox-esr python3-pip python3-tk python3-gdal gdal-bin" >> config/package-lists/software.list.chroot
 
 # ZBUDOWANIE PLIKU ISO Z SYSTEMEM OPERACYJNYM
 lb build 2>&1 | tee build.log
